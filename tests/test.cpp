acee056312c5cc3f87007d6b7902cb36cefe7f16
kirill050 <kirilltikhonov050@gmail.com>
2019-12-14 01:15:43 +0300

    14.12.19 1:15

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,22 +4,20 @@
 #include "header.hpp"
 
 TEST(Example, EmptyTest) {
-SharedPtr<char> pointer1();
-EXPEXT_EQ(pointer1.get(), nullptr);
+SharedPtr<char> pointer1;
+EXPECT_EQ(pointer1.get(), nullptr);
 
 char *test_ptr1 = new char;
 *test_ptr1 = 'a';
 
 SharedPtr<char> pointer2(test_ptr1);
-EXPEXT_EQ(pointer2.get(), test_ptr1);
-EXPEXT_EQ(pointer2.use_count(), 1);
-
-pointer1 = pointer2;
+EXPECT_EQ(pointer2.get(), test_ptr1);
+EXPECT_EQ(pointer2.use_count(), 1);
 
 SharedPtr<char> pointer3(pointer2);
-EXPEXT_EQ(pointer3.get(), pointer2.get());
-EXPEXT_EQ(pointer2.use_count(), 3);
-EXPEXT_EQ(pointer3.use_count(), 3);
+EXPECT_EQ(pointer3.get(), pointer2.get());
+EXPECT_EQ(pointer2.use_count(), 3);
+EXPECT_EQ(pointer3.use_count(), 3);
 
 char *test_ptr2 = new char;
 *test_ptr2 = 'b';
@@ -27,18 +25,18 @@ SharedPtr<char> pointer4(test_ptr2);
 EXPECT_EQ(*pointer4, *test_ptr2);
 
 pointer3 = pointer4;
-EXPEXT_EQ(pointer3.get(), pointer4.get());
+EXPECT_EQ(pointer3.get(), pointer4.get());
 
 pointer2.Swap(pointer3);
-EXPEXT_EQ(pointer2.get(), pointer4.get());
-EXPEXT_EQ(pointer3.get(), pointer1.get());
+EXPECT_EQ(pointer2.get(), pointer4.get());
+EXPECT_EQ(pointer3.get(), test_ptr1);
 
 pointer4.reset();
 EXPECT_EQ(pointer4.use_count(), 0);
 EXPECT_EQ(pointer4.get(), nullptr);
 
 pointer2.reset(test_ptr1);
-EXPEXT_EQ(pointer2.get(), test_ptr1);
+EXPECT_EQ(pointer2.get(), test_ptr1);
 }
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
