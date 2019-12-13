58e71ad34f39df680fcc07850002bde986d6ea67
kirill050 <kirilltikhonov050@gmail.com>
2019-12-13 21:34:11 +0300

    13.12.19 21:34

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,126 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2019 Kirill <your_email>
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+
+#include <string>
+#include <atomic>
+#include <map>
+
+using std::string;
+using std::map;
+using std::atomic_uint;
+
+template <typename T>
+class SharedPtr {
+public:
+    SharedPtr()
+    {
+        _ptr = nullptr;
+    }
+    explicit SharedPtr(T* ptr)
+    {
+        _ptr = ptr;
+        data_base[_ptr] = new atomic_uint(1);
+    }
+    explicit SharedPtr(const SharedPtr& r)
+    {
+        _ptr = r._ptr;
+        if (_ptr != nullptr)
+            (*data_base[_ptr])++;
+    }
+    explicit SharedPtr(SharedPtr&& r)
+    {
+        _ptr = r._ptr;
+        r._ptr = nullptr;
+    }
+    ~SharedPtr()
+    {
+        if (_ptr == nullptr)
+            return;
+        (*data_base[_ptr])--;
+        if ((*data_base[_ptr]) == 0)
+        {
+            delete data_base[_ptr];
+            data_base.erase(_ptr);
+            delete _ptr;
+        }
+        _ptr = nullptr;
+    }
+    auto opeartor=(const SharedPtr& r) -> SharedPtr&
+    {
+        if (&r == this)
+			return *this;
+        this->~SharedPtr();
+        _ptr = r._ptr;
+        if (_ptr != nullptr)
+            (*data_base[_ptr])++;
+        return *this;
+    }
+    auto opeartor=(SharedPtr&& r) -> SharedPtr&
+    {
+        if (&r == this)
+			return *this;
+		this->~SharedPtr();
+
+        _ptr = r._ptr;
+        r._ptr = nullptr;
+    }
+
+    // checks if the pointer points to an object
+    operator bool() const
+    {
+        return (_ptr != nullptr);
+    }
+    auto operator*() const -> T&
+    {
+        return *_ptr;
+    }
+    auto operator->() const -> T*
+    {
+        return _ptr;
+    }
+
+    auto get() -> T*
+    {
+        return _ptr;
+    }
+    void reset()
+    {
+        if (_ptr != nullptr)
+            if ((*data_base[_ptr]) != 0)
+                (*data_base[_ptr])--;
+        _ptr = nullptr;
+    }
+    void reset(T* ptr)
+    {
+        if (_ptr == nullptr)
+            return;
+        delete data_base[_ptr];
+        data_base.erase(_ptr);
+        delete _ptr;
+        _ptr = nullptr;
+    }
+    void Swap(SharedPtr& r)
+    {
+        T* temp;
+        temp = this->_ptr;
+        this->_ptr = r._ptr;
+        r._ptr = temp;
+        temp = nullptr;
+    }
+    // returns the number of SharedPtr objects
+    // that reference the same managed object
+    auto use_count() const -> size_t
+    {
+        if (_ptr == nullptr)
+            return 0;
+        size_t number = (*data_base[_ptr]);
+        return number;
+    }
+    static std::map <T*, std::atomic_uint*> data_base;
+private:
+    T* _ptr;
+};
 
 #endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,50 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2019 Kirill <your_email>
 
 #include <gtest/gtest.h>
+#include "header.hpp"
 
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+SharedPtr pointer1();
+EXPEXT_EQ(pointer1.get(), nullptr);
+
+char *test_ptr1 = new char;
+*test_ptr1 = 'a';
+
+SharedPtr<char> pointer2(test_ptr1);
+EXPEXT_EQ(pointer2.get(), test_ptr1);
+EXPEXT_EQ(pointer2.use_count(), 1);
+
+pointer1 = pointer2;
+
+SharedPtr<char> pointer3(pointer2);
+EXPEXT_EQ(pointer3.get(), pointer2.get());
+EXPEXT_EQ(pointer2.use_count(), 3);
+EXPEXT_EQ(pointer3.use_count(), 3);
+
+char *test_ptr2 = new char;
+*test_ptr2 = 'b';
+SharedPtr<char> pointer4(test_ptr2);
+EXPECT_EQ(*pointer4, *test_ptr2);
+
+pointer3 = pointer4;
+EXPEXT_EQ(pointer3.get(), pointer4.get());
+
+pointer2.Swap(pointer3);
+EXPEXT_EQ(pointer2.get(), pointer4.get());
+EXPEXT_EQ(pointer3.get(), pointer1.get());
+
+pointer4.reset();
+EXPECT_EQ(pointer4.use_count(), 0);
+EXPECT_EQ(pointer4.get(), nullptr);
+
+pointer3.reset(test_ptr1);
+EXPEXT_EQ(pointer1.get(), nullptr);
+EXPEXT_EQ(pointer3.get(), nullptr);
+EXPEXT_EQ(pointer1.use_count(), 0);
+EXPEXT_EQ(pointer3.use_count(), 0);
+
+}
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
